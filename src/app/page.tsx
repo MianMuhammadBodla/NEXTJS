3251ddf8cf4395ae7f6a4222ae115b4ee2c6f86d
MianMuhammadBodla <mianmuhammadbodla@gmail.com>
2023-10-10 00:42:00 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import Button from "@/components/Buttons"
+
 export default function Home() {
   return (
-        <div>Hello World</div>
+    <main className="flex min-h-screen flex-col items-center p-24 gap-5">
+    <h1 className="font-bold text-6xl text-red-600">
+      Hello World 
+    </h1>
+    <p>
+     Greedings Everyone,      My Name is Muhammad I am Second year student of Fsc Pre Medical at Forman Christian College Lahore
+     I have also YouTube Channel name as "MianMuhammadBodla"
+     I am also learning TypeScript and Nextjs from PIAIC
+    </p>
+    <Button />
+        <div>Subscribe  <Button />
+        </div>
+      
+    </main>
   );
 }
